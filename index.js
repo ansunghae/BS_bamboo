54e58e6ce4fd0701031dec2292e885ae86932284
ansunghae <ahshss903@gmail.com>
2023-03-10 09:17:04 +0900

    230310

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = 80
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/view/html/index.html')
+  res.sendFile(__dirname + '/public/html/index.html')
 });
+
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/public/html/chat.html')
+})
 
 app.listen(port , () => {
   console.log("Server Open at http://localhost:", port);
